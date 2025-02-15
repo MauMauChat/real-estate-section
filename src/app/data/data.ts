74d579e66dfcb1d26eb6cb53141ec6bc0f9f581d
MauMauChat <lucaan@edu.aau.at>
2025-02-15 13:46:08 +0100

    Add data.ts
    Seperate data file for the arrays

---
+++ src/app/data/data.ts
@@ -0,0 +1,6 @@
+// data.ts
+export const categories = ['Houses', 'Rooms/Apartments'];
+export const types = ['Detached House', 'Apartment', 'Penthouse'];
+export const cities = ['Graz', 'Vienna', 'Innsbruck'];
+export const provinces = ['Steiermark', 'Tirol', 'Kärnten'];
+
